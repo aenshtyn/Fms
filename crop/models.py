fa84e12359738775d193f89e7402d09040739344
Mohamed <demahom93@yahoo.co.uk>
2024-04-06 17:54:27 +0300

    updates on the crop modukle

--- crop/models.py
+++ crop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from FMS.mixins import NameMixin, AgeMixin, GenderMixin
+
+class Crop(models.Model):
+    name = models.CharField(max_length=100)
+    variety = models.CharField(max_length=100, blank=True, null=True)
+    planting_date = models.DateField()
+    harvest_date = models.DateField()
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.CharField(max_length=50)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    notes = models.TextField(blank=True)
+
+    feeds_livestock = models.BooleanField(default=False)  # Indicates if the crop becomes feed for livestock
+
+    # Additional fields specific to crop management can be added here
+
+    def __str__(self):
+        if self.variety:
+            return f"{self.name} - {self.variety}"
+        return self.name
