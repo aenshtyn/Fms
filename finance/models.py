3466db6921d3755f4ad3c9de8354b78cfd34b1ab
Mohamed <demahom93@yahoo.co.uk>
2024-04-08 05:39:19 +0300

    models for the finance module

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,106 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+
+class Budget(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Expense(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+
+class Income(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+
+class Invoice(models.Model):
+    customer = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+class Payment(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Tax(models.Model):
+    name = models.CharField(max_length=100)
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Asset(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Liability(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+class FinancialReport(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    content = models.TextField()
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+class IncomeCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+class Loan(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Investment(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Interest(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+class Credit(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+class FinancialGoal(models.Model):
+    name = models.CharField(max_length=100)
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    deadline = models.DateField()
+
+class FinancialForecast(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    content = models.TextField()
+
+class FinancialInstitution(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+
+class ExpenseApproval(models.Model):
+    expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
+    approver = models.ForeignKey(User, on_delete=models.CASCADE)
+    approved = models.BooleanField(default=False)
+
+class Categorization(models.Model):
+    name = models.CharField(max_length=100)
+    category_type = models.CharField(max_length=20) 
--- finance/views.py
+++ finance/views.py
@@ -1,8 +1,92 @@
-from django.shortcuts import render
-from rest_framework import status, viewsets
+# finance/views.py
+from rest_framework import viewsets
+from .models import Transaction, Budget, Expense, Income, Invoice, Payment, Account, Tax, Asset, Liability, FinancialReport, ExpenseCategory, IncomeCategory, Loan, Investment, Interest, Credit, FinancialGoal, FinancialForecast, FinancialInstitution, ExpenseApproval, Categorization
+from .serializers import TransactionSerializer, BudgetSerializer, ExpenseSerializer, IncomeSerializer, InvoiceSerializer, PaymentSerializer, AccountSerializer, TaxSerializer, AssetSerializer, LiabilitySerializer, FinancialReportSerializer, ExpenseCategorySerializer, IncomeCategorySerializer, LoanSerializer, InvestmentSerializer, InterestSerializer, CreditSerializer, FinancialGoalSerializer, FinancialForecastSerializer, FinancialInstitutionSerializer, ExpenseApprovalSerializer, CategorizationSerializer
 
-# Create your views here.
-class FinanceViewSet(viewsets.ModelViewSet):
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
 
-    def list(self, request):
-        pass
+class BudgetViewSet(viewsets.ModelViewSet):
+    queryset = Budget.objects.all()
+    serializer_class = BudgetSerializer
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+class IncomeViewSet(viewsets.ModelViewSet):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+class TaxViewSet(viewsets.ModelViewSet):
+    queryset = Tax.objects.all()
+    serializer_class = TaxSerializer
+
+class AssetViewSet(viewsets.ModelViewSet):
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+
+class LiabilityViewSet(viewsets.ModelViewSet):
+    queryset = Liability.objects.all()
+    serializer_class = LiabilitySerializer
+
+class FinancialReportViewSet(viewsets.ModelViewSet):
+    queryset = FinancialReport.objects.all()
+    serializer_class = FinancialReportSerializer
+
+class ExpenseCategoryViewSet(viewsets.ModelViewSet):
+    queryset = ExpenseCategory.objects.all()
+    serializer_class = ExpenseCategorySerializer
+
+class IncomeCategoryViewSet(viewsets.ModelViewSet):
+    queryset = IncomeCategory.objects.all()
+    serializer_class = IncomeCategorySerializer
+
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+class InvestmentViewSet(viewsets.ModelViewSet):
+    queryset = Investment.objects.all()
+    serializer_class = InvestmentSerializer
+
+class InterestViewSet(viewsets.ModelViewSet):
+    queryset = Interest.objects.all()
+    serializer_class = InterestSerializer
+
+class CreditViewSet(viewsets.ModelViewSet):
+    queryset = Credit.objects.all()
+    serializer_class = CreditSerializer
+
+class FinancialGoalViewSet(viewsets.ModelViewSet):
+    queryset = FinancialGoal.objects.all()
+    serializer_class = FinancialGoalSerializer
+
+class FinancialForecastViewSet(viewsets.ModelViewSet):
+    queryset = FinancialForecast.objects.all()
+    serializer_class = FinancialForecastSerializer
+
+class FinancialInstitutionViewSet(viewsets.ModelViewSet):
+    queryset = FinancialInstitution.objects.all()
+    serializer_class = FinancialInstitutionSerializer
+
+class ExpenseApprovalViewSet(viewsets.ModelViewSet):
+    queryset = ExpenseApproval.objects.all()
+    serializer_class = ExpenseApprovalSerializer
+
+class CategorizationViewSet(viewsets.ModelViewSet):
+    queryset = Categorization.objects.all()
+    serializer_class = CategorizationSerializer
